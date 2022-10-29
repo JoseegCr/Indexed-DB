6b65f74ab3c2a94bfd3a5d303132677168676fe7
JoseegCr <joseegccr@gmail.com>
2022-10-29 11:57:39 -0600

    IndexedDB

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -1,6 +1,18 @@
 
 // indexedDB: Reforzamiento
 
+let request = window.indexedDB.open('mi-database', 1);
 
+// Se actualiza cuando se crea o se sube de versión en la db
+request.onupgradeneeded = event =>{
+
+    console.log('Actualización de la DB');
+
+    let db = event.target.result;
+
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+    });
+};
 
 

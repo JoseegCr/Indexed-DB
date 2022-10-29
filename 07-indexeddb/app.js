af426e34cc61552aad1db981f5709a70ea9fe2b3
JoseegCr <joseegccr@gmail.com>
2022-10-29 11:59:19 -0600

    Manejo de errores e insercion de registros

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -15,4 +15,45 @@ request.onupgradeneeded = event =>{
     });
 };
 
+// Manejo de errores
+request.onerror = event => {
+    console.log('DB error: ', event.target.error);
+};
+
+
+// Insertar datos 
+request.onsuccess = event => {
+
+    let db = event.target.result;
+
+    let heroesData = [
+        { id: '1111', heroe: 'Spiderman', mensaje: 'Aquí está su amigo Spiderman'},
+        { id: '2222', heroe: 'Iron-man', mensaje: 'Aquí en mi nuevo Mark 50'}
+    ];
+
+
+    let heroesTransaction = db.transaction('heroes', 'readWrite');
+
+    heroesTransaction.onerror = event => {
+        console.log('Error guardando', event.target.error);
+    };
+
+    // Informar el exito de la transacción
+    heroesTransaction.onComplete = event => {
+        console.log('Transacción hecha', event);
+    };
+
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    for (let heroe of heroesData)
+    {
+        heroesData.add(heroe);
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado a la base de datos');
+    };
+
+};
+
 

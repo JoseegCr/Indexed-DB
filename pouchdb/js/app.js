7a4c73a720bb555a9fdf31b7432954e09138fa76
JoseegCr <joseegccr@gmail.com>
2022-10-29 12:22:09 -0600

    Editar y Borrar TODOS

--- pouchdb/js/app.js
+++ pouchdb/js/app.js
@@ -17,6 +17,9 @@
 
   // We have to create a new todo document and enter it in the database
   function addTodo(text) {
+
+    if(trimmedText <= 0) return;
+
     var todo = {
       _id: new Date().toISOString(),
       title: text,
@@ -47,15 +50,30 @@
   }
 
   function checkboxChanged(todo, event) {
+    todo.completed = event.target.checked;
+    db.put(todo);
   }
 
   // User pressed the delete button for a todo, delete it
   function deleteButtonPressed(todo) {
+    db.remove(todo);
   }
 
   // The input box when editing a todo has blurred, we should save
   // the new title or delete the todo if the title is empty
   function todoBlurred(todo, event) {
+    var trimmedText = event.target.value.trim();
+
+
+    if(!trimmedText)
+    {
+      db.remove(todo);
+    }
+    else
+    {
+      todo.title = trimmedText;
+      db.put(todo);
+    }
   }
 
   // Initialise a sync with the remote server
